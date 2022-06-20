daa964f90b190f88ace76faa6196a0ef949a594d
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-06-20 17:06:44 +0700

    feat: add constants

---
+++ Learning-Reactive/Utilities/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  Learning-Reactive
+//
+//  Created by Nuttapon Buaban on 20/6/2565 BE.
+//
+
+import Foundation
+
+enum ThaiDigit: String {
+    case ZERO = "๐"
+    case ONE = "๑"
+    case TWO = "๒"
+    case THREE = "๓"
+    case FOUR = "๔"
+    case Five = "๕"
+    case SIX = "๖"
+    case SEVEN = "๗"
+    case EIGHT = "๘"
+    case NINE = "๙"
+}
