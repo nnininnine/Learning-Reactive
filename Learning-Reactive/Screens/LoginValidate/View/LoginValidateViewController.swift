ddbd4d4b9c5d1858c1b3def87c2d52e149c2bc5e
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-06-21 13:16:37 +0700

    feat: add LoginValidateViewController

---
+++ Learning-Reactive/Screens/LoginValidate/View/LoginValidateViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginValidateViewController.swift
+//  Learning-Reactive
+//
+//  Created by Nuttapon Buaban on 21/6/2565 BE.
+//
+
+import Foundation
+import UIKit
+
+class LoginValidateViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
